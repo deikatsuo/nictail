23e60090a84a411231d3fa1bdcf8baa61b0b5234
Deri Herdianto <derryherdianto@gmail.com>
2016-05-17 23:50:40 +0700

    App Register

--- app/App/Register/Register.php
+++ app/App/Register/Register.php
@@ -2,13 +2,28 @@
 namespace App\Register;
 
 use System\Core\App;
+use System\Core\Country;
+Use GeoIp2\Database\Reader;
 
 Class Register Extends App {
 	public $render=[];
 	public function index() {
+		$vCountryIso="";
+		try {
+			$readIp=new Reader(_root."/GeoLite2-Country.mmdb");
+			$vCountry=$readIp->Country($_SERVER['REMOTE_ADDR']);
+			$vCountryIso=$vCountry->country->isoCode;
+		}
+		catch (\GeoIp2\Exception\AddressNotFoundException $e) {
+			//Error atau IP tidak terdaftar didatabase
+		}
+
 		$this->render=[
-			'title'		=> 'Daftar akun baru'
+			'title'			=> 'Daftar akun baru',
+			'country'		=> new \System\Core\Country(),
+			'vCountryIso'	=> $vCountryIso
 		];
+		$this->container->get('app.asset-manager')->addTo('Register',['register.js']);
 		return $this;
 	}
 
