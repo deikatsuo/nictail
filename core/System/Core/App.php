4fe4cc7554b4d439abfc252a665d39301b865ede
Deri Herdianto <derryherdianto@gmail.com>
2016-05-17 23:43:13 +0700

    App Core

--- core/System/Core/App.php
+++ core/System/Core/App.php
@@ -3,33 +3,36 @@ namespace System\Core;
 
 use Symfony\Component\HttpFoundation\Response;
 
+
 Abstract Class App {
 	public $container=[];
 	public $place=[
 		'place_after_menu_navbar'		=> '',
 		'place_after_left_menu'			=> ''
 	];
+	protected $current_app;
+	protected $render_merge=[];
 
 	public function __construct() {
 		// ---
+		$this->current_app=end(explode('/',$this->config()['path']));
 	}
 
 	public function show() {
 		$twig=$this->container->get('twig.environment');
-		$current_app=end(explode('/',$this->config()['path']));
-		$this->container->get('twig.loader')->addPath($this->config()['path'].'/template',$current_app);
+		
+		$this->container->get('twig.loader')->addPath($this->config()['path'].'/template',$this->current_app);
 
-		return new Response($twig->render('@'.$current_app.'/'.$this->config()['template'],$this->prepare()));
+		return new Response($twig->render('@'.$this->current_app.'/'.$this->config()['template'],$this->prepare()));
 	}
 
 	# @Return array
 	private function prepare() {
-		$asset=$this->container->get('app.asset-manager');
-		$asset->add(['pure.css','jquery-2.2.3.min.js']);
+		$this->container->get('app.asset-manager')->add(['global.css','materialize.min.css','jquery-2.2.3.min.js','materialize.min.js','global.js']);
 
 		// Prepare
 		$render=[
-			'head_asset'	=>	$asset->load()
+			'head_asset'	=>	$this->container->get('app.asset-manager')->load()
 		];
 		return array_merge($this->place,$render,$this->render);
 	}
