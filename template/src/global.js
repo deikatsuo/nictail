4663c423871dd4d776a41cc1abda92c2555cc99b
Deri Herdianto <derryherdianto@gmail.com>
2016-06-20 01:49:44 +0700

    update

--- template/src/global.js
+++ template/src/global.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	$('.dropdown-button').dropdown({
-		inDuration: 300,
-		outDuration: 225,
-		constrain_width: false, // Does not change width of dropdown to that of the activator
-		hover: false, // Activate on hover
-		gutter: -15, // Spacing from edge
-		belowOrigin: true, // Displays dropdown below the button
-		alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    });
+
 });
