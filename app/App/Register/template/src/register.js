b75a555aef5f87ba1e4056d90f2c69a43df639d6
Deri Herdianto <derryherdianto@gmail.com>
2016-08-18 12:17:37 +0700

    Update

--- app/App/Register/template/src/register.js
+++ app/App/Register/template/src/register.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 	$('#reg_form').submit(function(e){
 		e.preventDefault();
+		if($('#g-recaptcha-response').val() == '') {
+			$('.g-recaptcha-c').addClass('uk-form-danger');
+			$('.g-recaptcha-c i').css('display','block');
+			return false;
+		}
+		$('.g-recaptcha-c i').css('display','none');
 		var url=$(this).attr('action');
 		var send=$.post(url, $(this).serialize(),'json');
 		send.done(function(data){
 			data=$.parseJSON(data);
 			$('#reg_form').find('*').removeClass('uk-form-danger');
 			if(!data.success) {
+				grecaptcha.reset();
 				var error=data.error;
 				for(var keys in error) {
 					$('#'+keys).addClass('uk-form-danger');
